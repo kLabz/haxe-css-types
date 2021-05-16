f29209102a9098e4d5a13945a446c16263ce26a4
k <k@klabz.org>
2021-05-16 11:12:16 +0200

    0.9.0 - Rewrite IntOrString, add css writer
    Still need README and tests before 1.0.0

--- src/css/GlobalValue.hx
+++ src/css/GlobalValue.hx
@@ -1,7 +1,12 @@
 package css;
 
+@:transitive
 @:enum abstract GlobalValue(String) to String {
 	var Inherit = "inherit";
 	var Initial = "initial";
 	var Unset = "unset";
+
+	public static function Var(v:String):GlobalValue {
+		return cast 'var($v)';
+	}
 }
